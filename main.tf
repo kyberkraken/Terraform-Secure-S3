69bc30109679593bd56272112da4fa164463c7c6
Abhishek Gopal <abhishekgopal@Abhisheks-MacBook-Air.local>
2026-08-22 21:44:56 +0530

    added terraform github action, terraform file

--- main.tf
+++ main.tf
@@ -1,2 +1,23 @@
-t
+terraform {
+    required_providers {
+        aws = {
+            source = "hashicorp/aws"
+            version = "~>5.0"
+        }
+    }
+}
+
+provider "aws" {
+    region= "ap-south-1"
+}
+
+resource "random_id" "bucket_suffix"{
+    byte_length=4
+}
+
+resource "aws_s3_bucket" "s3_example" {
+    bucket="unique_bucket_${random_id.bucket_suffix.hex}"
+
+}
+
 
